ede55012794f0cf3b2274364591674696e668672
Juan Camilo <juancmz98@gmail.com>
2021-06-23 16:24:08 -0500

    image resource added to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
       {
         test: /\.s?css$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource'
       }
     ]
   },
