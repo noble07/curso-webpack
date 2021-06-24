80ce903d1a5a3acf7e1c110f1f80c8e9f0dfeee7
Juan Camilo <juancmz98@gmail.com>
2021-06-24 10:26:15 -0500

    optimizer for assets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import Template from './templates/Template.js'
-import './styles/main.css'
-import './styles/main.scss'
+import Template from '@templates/Template.js'
+import '@styles/main.css'
+import '@styles/main.scss'
 ;(async function App() {
   const main = null || document.getElementById('main')
   main.innerHTML = await Template()
--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,24 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: '[name].[contenthash].js',
     assetModuleFilename: 'assets/images/[hash][ext][query]'
   },
   resolve: {
-    extensions: ['.js']
+    extensions: ['.js'],
+    alias: {
+      '@utils': path.resolve(__dirname, 'src/utils/'),
+      '@templates': path.resolve(__dirname, 'src/templates/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@images': path.resolve(__dirname, 'src/assets/images/')
+    }
   },
   module: {
     rules: [
@@ -37,7 +45,7 @@ module.exports = {
           options: {
             limit: 10000,
             MimeType: 'application/font-woff',
-            name: '[name].[ext]',
+            name: '[name].[contenthash].[ext]',
             outputPath: './assets/fonts/',
             publicPath: './assets/fonts/',
             esModule: false
@@ -52,7 +60,9 @@ module.exports = {
       template: './public/index.html',
       filename: './index.html'
     }),
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].[contenthash].css'
+    }),
     new CopyPlugin({
       patterns: [
         {
@@ -61,5 +71,9 @@ module.exports = {
         }
       ]
     })
-  ]
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()]
+  }
 }
